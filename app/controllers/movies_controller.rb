4386e1d58b8658e7667bf64385ed62165b855bd1
Yi Liu <Jacob@mail-alt.suddenlink.net>
2018-09-28 00:11:52 -0500

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,8 +42,6 @@ class MoviesController < ApplicationController
       redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
     end
   end
-  
-  end
 
   def new
     # default: render 'new' template
